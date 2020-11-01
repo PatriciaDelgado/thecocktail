4b75cd91e02c61ee7aad88f32c634f400c08aa2a
pdelgado <pdelgado@mrmilu.com>
2020-11-01 23:54:24 +0100

    first iteration fixed with content body 1270 item tabs

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,14 @@
-// This is the entrypoint for javascript
-// Import all your js files here
-// ...
+$(document).ready(function() {
+  $('.header__menu > li > a').click(function(event) {
+    event.preventDefault();
+    var active_menu = $('.header__menu > li.active > a').attr('href');
+    var actived_navigation = $('.header__menu > li.active');
+    actived_navigation.removeClass('active');
+    $(this).parents('li').addClass('active');
+    $(active_menu).removeClass('active');
+    $(active_menu).addClass('hide');
+    var tab_selector = $(this).attr('href');
+    $(tab_selector).removeClass('hide');
+    $(tab_selector).addClass('active');
+  });
+});
