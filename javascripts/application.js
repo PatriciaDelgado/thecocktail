57016e10f0868943893427cfa0d57461f3df8af8
pdelgado <pdelgado@mrmilu.com>
2020-11-02 12:14:54 +0100

    second iteration fixed

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('.header__menu > li > a').click(function(event) {
-    event.preventDefault();
+  $('.header__menu > li > a').click(function(e) {
+    e.preventDefault();
     var active_menu = $('.header__menu > li.active > a').attr('href');
     var actived_navigation = $('.header__menu > li.active');
     actived_navigation.removeClass('active');
@@ -17,15 +17,32 @@ $(document).ready(function() {
 
     var $this = $(this);
 
+
     if ($this.next().hasClass('show')) {
       $this.next().removeClass('show');
       $this.next().slideUp(350);
+      $this.toggleClass('active');
     } else {
       $this.parent().parent().find('li .inner').removeClass('show');
       $this.parent().parent().find('li .inner').slideUp(350);
       $this.next().toggleClass('show');
       $this.next().slideToggle(350);
+      $this.toggleClass('active');
     }
   });
+
+  $('.more__info').click( function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.toggleClass('active');
+    $("#subtab").slideToggle(350);
+  });
+
+  $('.content__list-item').click(function(e){
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
 });
 
