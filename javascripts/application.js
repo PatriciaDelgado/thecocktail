d53f020bb25e8ea5434df410b9ace57d177381ea
pdelgado <pdelgado@mrmilu.com>
2020-11-02 01:27:52 +0100

    second tab iteration

--- javascripts/application.js
+++ javascripts/application.js
@@ -11,4 +11,21 @@ $(document).ready(function() {
     $(tab_selector).removeClass('hide');
     $(tab_selector).addClass('active');
   });
+
+  $('.toggle').click(function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+    } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
+    }
+  });
 });
+
